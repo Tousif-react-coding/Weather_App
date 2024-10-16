5592490075b0edcc722bf022eaa3971879eb9362
Tousif-react-coding <tousifa925@gmail.com>
2024-10-16 18:40:10 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000 // Default to port 3000 if PORT is not set
+  }
 })
